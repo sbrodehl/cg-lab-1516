6a561be474ce439eb4e14eac383b27589697279e
clamber <clamber@students.uni-mainz.de>
2016-03-14 13:54:40 +0100

    added parameterized Torus.

--- Parametrics.cpp
+++ Parametrics.cpp
@@ -10,6 +10,21 @@ QVector3D Parametrics::getPoint(double u, double v) {
     return QVector3D();
 }
 
+QVector3D Parametrics::parameterizedTorus(float u, float v){
+    // u and v in [0, 1]
+    float u_scaled = u*2*PI;
+    float v_scaled = v*2*PI;
+    // hard coded main circle radius R and sub circle radius r
+    float R = 1;
+    float r = 1;
+
+    float x = (R + r*cos(v_scaled))*cos(u_scaled);
+    float y = (R + r*cos(v_scaled))*sin(u_scaled);
+    float z = r*sin(v_scaled);
+
+    return QVector3D(x, y, z);
+}
+
 Parametrics::Parametrics() {
     // constructor
 }
