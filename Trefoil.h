eb79978444faa37fcb213f7a3a3e9e17932f1b0f
Sebastian Brodehl <sbrodehl@students.uni-mainz.de>
2016-03-15 12:23:15 +0100

    Updated normals and points.

--- Trefoil.h
+++ Trefoil.h
@@ -1,4 +1,3 @@
-
 #ifndef CG_LAB_1516_TREFOIL_H
 #define CG_LAB_1516_TREFOIL_H
 
@@ -24,32 +23,36 @@ class Trefoil : public Parametrics {
 
 private:
     // hard coded sub circle radius r
-    float a = 2.0f;
+    float a = 5.0f;
     float b = 2.0f;
     float d = 10.0f;
     float q = 3.0f;
     float p = 2.0f;
 
+    double scaled(double x) {
+        return x * 2 * PI;
+    }
+
 public:
     double x(double u, double v) {
-        float u_scaled = (float) (u * 2 * PI);
-        float v_scaled = (float) (v * 2 * PI);
+        float theta = (float) scaled(u);
+        float phi = (float) scaled(v);
 
-        return ((a * sin(q * u_scaled) + d) * sin(p * u_scaled) ) + b * sin(p * u_scaled + v_scaled);
+        return ((a * sin(q * theta) + d) * sin(p * theta)) + b * sin(p * theta + phi);
     }
 
     double y(double u, double v) {
-        float u_scaled = (float) (u * 2 * PI);
-        float v_scaled = (float) (v * 2 * PI);
+        float theta = (float) scaled(u);
+        float phi = (float) scaled(v);
 
-        return ((a * sin(q * u_scaled) + d) * cos(p * u_scaled) ) + b * cos(p * u_scaled + v_scaled);
+        return ((a * sin(q * theta) + d) * cos(p * theta)) + b * cos(p * theta + phi);
     }
 
     double z(double u, double v) {
-        float u_scaled = (float) (u * 2 * PI);
-        float v_scaled = (float) (v * 2 * PI);
+        float theta = (float) scaled(u);
+        float phi = (float) scaled(v);
 
-        return a * cos(q * u_scaled) + b * cos (q * u_scaled + v_scaled);
+        return a * cos(q * theta) + b * cos(q * theta + phi);
     }
 
     QVector3D getPoint(double u, double v) {
@@ -59,9 +62,21 @@ public:
     }
 
     QVector3D getNormal(double u, double v) {
-        return QVector3D(0,
-                         0,
-                         0);
+        float theta = (float) scaled(u);
+        float phi = (float) scaled(v);
+
+        QVector3D dtheta((float) (b*p*cos(p*theta+phi)+a*q*cos(q*theta)*sin(p*theta)+p*cos(p*theta)*(d+a*sin(q*theta))),
+                     (float) (a*q*cos(p*theta)*cos(q*theta)-p*sin(p*theta)*(d+a*sin(q*theta))-b*p*sin(p*theta+phi)),
+                     (float) (-q * (a * sin(q * theta) + b * sin(q * theta + phi))));
+
+        QVector3D dphi((float) (b * cos(p * theta + phi)),
+                     (float) (-b * sin(p * theta + phi)),
+                     (float) (-b * sin(q * theta + phi)));
+
+        QVector3D n = QVector3D::crossProduct(dtheta, dphi);
+        n.normalize();
+
+        return n;
     }
 
     std::vector<QPair<QVector3D, QVector3D> > getSegments() {
